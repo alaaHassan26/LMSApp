e1e5b49deac3538b0af1b65a7525c6d32ff6d767
alaaHassan26 <ah7657157@gmail.com>
2024-09-11 00:30:44 +0300

    fix android 13 and above

--- lib/features/home/presentation/manger/download_image_cubit/download_image_cubit.dart
+++ lib/features/home/presentation/manger/download_image_cubit/download_image_cubit.dart
@@ -2,10 +2,8 @@ import 'dart:typed_data';
 
 import 'package:bloc/bloc.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
-
 import 'package:dio/dio.dart';
 import 'package:lms/features/home/presentation/manger/download_image_cubit/download_image_state.dart';
-
 import 'package:permission_handler/permission_handler.dart';
 
 class DownloadCubit extends Cubit<DownloadState> {
@@ -14,9 +12,8 @@ class DownloadCubit extends Cubit<DownloadState> {
   Future<void> downloadImage(String imageUrl) async {
     emit(DownloadLoading());
 
-    PermissionStatus permissionStatus = await Permission.storage.request();
-
-    if (permissionStatus.isGranted) {
+    // التحقق من إصدار أندرويد الحالي لمعالجة الأذونات المناسبة
+    if (await _requestPermission()) {
       try {
         Dio dio = Dio();
         final response = await dio.get(imageUrl,
@@ -33,9 +30,33 @@ class DownloadCubit extends Cubit<DownloadState> {
       } catch (e) {
         emit(DownloadFailure('Download failed: ${e.toString()}'));
       }
-    } else if (permissionStatus.isDenied ||
-        permissionStatus.isPermanentlyDenied) {
-      emit(DownloadFailure('Permission denied to access storage.'));
+    } else {
+      emit(DownloadFailure('Permission denied to access media.'));
+    }
+  }
+
+  Future<bool> _requestPermission() async {
+    // التحقق من إصدار Android 13 وما بعده لاستخدام أذونات الوسائط الجديدة
+    if (await Permission.storage.isGranted) {
+      return true;
+    } else if (await Permission.photos.isGranted) {
+      return true;
+    } else {
+      // طلب الأذونات بناءً على إصدار أندرويد
+      PermissionStatus storagePermission;
+      if (await Permission.storage.isRestricted ||
+          await Permission.storage.isPermanentlyDenied ||
+          await Permission.photos.isRestricted) {
+        return false;
+      }
+
+      // إذا كان الجهاز Android 13 وما بعده، استخدم إذن الصور
+      if (await Permission.photos.request().isGranted) {
+        return true;
+      } else {
+        storagePermission = await Permission.storage.request();
+        return storagePermission.isGranted;
+      }
     }
   }
 }
